97c4c8fc08ced2887544809d8565932c9b4c811e
Kasun Tharanga <kasun.tharanga@infor.com>
2021-06-15 19:54:49 +0530

    added console logs

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
+    console.log('page loaded.');
     res.send('IT Works');
 })
 
 app.get('/new', (req, res)=>{
+    console.log('new page loaded.');
     res.send('New also Works');
 })
 
 app.get('/help', (req, res)=>{
+    console.log('help page loaded.');
     res.send('help page also Works');
 })
 
