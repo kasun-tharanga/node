0fbd62ddef4b77d30b3ca15b7c5afa74b125be60
Kasun Tharanga <kasun.tharanga@infor.com>
2021-06-15 17:14:26 +0530

    added help, new pages

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ app.get('/', (req, res)=>{
     res.send('IT Works');
 })
 
+app.get('/new', (req, res)=>{
+    res.send('New also Works');
+})
+
+app.get('/help', (req, res)=>{
+    res.send('help page also Works');
+})
+
 app.listen(process.env.PORT || 5000);
 module.exports = app;
